03db172ca3fbbfa5768eae27366fb6ba4e64086a
NickHackman <snickhackman@gmail.com>
2021-06-29 01:17:46 -0400

    feat(api): initial project setup
    Setup with Express, Socket.io, Prettier, Eslint, and Typesript.

---
+++ api/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: "./tsconfig.json" },
+  plugins: ["@typescript-eslint"],
+  extends: ["airbnb-typescript", "prettier"],
+};
---
+++ api/README.md
@@ -0,0 +1,3 @@
+# Dev Chat Api
+
+A simple Chat app backend Api utilizing socket.io and express.
