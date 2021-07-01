e7aa84242508b83ddcbf200e888d986fcc4be7c9
obeymyshinnyrod <miswah.md@gmail.com>
2021-07-02 01:20:44 +0530

    added login/logout functionality via auth0 also fetching of profile info #16

--- web/src/app/logout/logout.component.ts
+++ web/src/app/logout/logout.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  styleUrls: ['./logout.component.css'],
 })
 export class LogoutComponent implements OnInit {
-
-  constructor() { }
+  constructor(public auth: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.auth.logout({ returnTo: window.location.origin });
   }
-
 }
