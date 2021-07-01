bf93b203b9f71cbade016ea16b398a0e61718250
obeymyshinnyrod <miswah.md@gmail.com>
2021-07-01 22:31:08 +0530

    added all the dependencies needed

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatboxComponent } from './chatbox/chatbox.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UsersComponent } from './users/users.component';
+import { CurrentUserComponent } from './current-user/current-user.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ChatComponent } from './chat/chat.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatboxComponent,
+    ProfileComponent,
+    UsersComponent,
+    CurrentUserComponent,
+    LogoutComponent,
+    ChatComponent,
+    NavigationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
